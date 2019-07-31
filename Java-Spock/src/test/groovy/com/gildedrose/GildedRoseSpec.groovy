04efefe5bad2481dcdfd61e243be2cf6e3fa612a
Donatas Mockus <Donatas.Mockus@4finance.com>
2019-07-31 15:32:06 +0300

    test for common items

--- Java-Spock/src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
+++ Java-Spock/src/test/groovy/com/gildedrose/GildedRoseSpec.groovy
@@ -2,24 +2,49 @@ package com.gildedrose
 
 import spock.lang.Specification
 
-/**
- * Spock unit tests.
- */
 class GildedRoseSpec extends Specification {
 
-    def "should update quality correctly"() {
-
-        given: "some items"
-        Item[] items = [new Item("foo", 0, 0)];
-
-        and: "the application with these items"
-        GildedRose app = new GildedRose(items);
-
+    def "should decrease quality by 1 for common item"() {
+        given: "common item"
+            Item[] items = [new Item("common item", 15, 33)];
+        and: "the application with this item"
+            GildedRose app = new GildedRose(items);
         when: "updating quality"
-        app.updateQuality();
-
-        then: "the quality is correct"
-        app.items[0].name == "fixme"
+            app.updateQuality();
+        then: "quality increased by 1"
+            app.items[0].quality == 32
     }
 
+    def "should decrease quality by 2 when sellIn is negative for common item"() {
+        given: "common item with negative sellIn"
+            Item[] items = [new Item("common item", -1, 28)];
+        and: "the application with this item"
+            GildedRose app = new GildedRose(items);
+        when: "updating quality"
+            app.updateQuality();
+        then: "quality increased by 2"
+            app.items[0].quality == 26
+    }
+
+    def "should not decrease quality below zero"() {
+        given: "common item with zero quality"
+            Item[] items = [new Item("common item", 4, 0)];
+        and: "the application with this item"
+            GildedRose app = new GildedRose(items);
+        when: "updating quality"
+            app.updateQuality();
+        then: "quality did not change"
+            app.items[0].quality == 0
+    }
+
+    def "should decrease sellIn by 1"() {
+        given: "common item"
+            Item[] items = [new Item("common item", 11, 23)];
+        and: "the application with this item"
+            GildedRose app = new GildedRose(items);
+        when: "updating quality"
+            app.updateQuality();
+        then: "the quality is correct"
+            app.items[0].sellIn == 10
+    }
 }
